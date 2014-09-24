6db4f1db4e330332bc864abb577b725c8f2c8bb5
maemual <maemual@gmail.com>
2014-09-24 15:55:32 +0800

    Add index handle

--- pic.go
+++ pic.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"html/template"
 	"os"
+
+	"log"
+	"net/http"
 
 	"github.com/codegangsta/cli"
 )
+
+var indexTemplate = template.Must(template.ParseFiles("index.html"))
+
+func indexHandle(w http.ResponseWriter, r *http.Request) {
+	if err := indexTemplate.Execute(w, nil); err != nil {
+		log.Fatal("Execute: ", err.Error())
+		return
+	}
+}
 
 func main() {
 	app := cli.NewApp()
@@ -21,6 +34,9 @@ func main() {
 		if _, err := os.Stat(c.String("path")); err != nil && !os.IsExist(err) {
 			os.MkdirAll(c.String("path"), os.ModePerm)
 		}
+
+		http.HandleFunc("/", indexHandle)
+		http.ListenAndServe(":8090", nil)
 	}
 
 	app.Run(os.Args)
